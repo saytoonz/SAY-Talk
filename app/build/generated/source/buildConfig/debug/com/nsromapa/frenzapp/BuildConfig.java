585290748df3d0b12da4b996163893918d868f3c
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-02 13:53:56 +0000

    Upgrading

--- app/build/generated/source/buildConfig/debug/com/nsromapa/frenzapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/nsromapa/frenzapp/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.nsromapa.saytoonz.frenzapp";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 5;
   public static final String VERSION_NAME = "1.0.5";
 }
